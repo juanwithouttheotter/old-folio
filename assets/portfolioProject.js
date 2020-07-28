1eff124df6b542e7d8816bc4b8461b72c6b00206
Juan Osorio <juan.michael.osorio@gmail.com>
2020-07-28 12:24:18 -0500

    added images for markdown and cele-o-knowledge

--- assets/portfolioProject.js
+++ assets/portfolioProject.js
@@ -5,11 +5,6 @@ const projects = [
         "altTag": "Password generator website screenshot"
     },
     {
-        "urlTag": "morndusk-planner",
-        "imgRoute": "morndusk-planner-image.png",
-        "altTag": "Day planner website screenshot"
-    },
-    {
         "urlTag": "weather-today-or-tomorrow",
         "imgRoute": "weather-today-or-tomorrow-image.png",
         "altTag": "weather app screenshot"
@@ -25,6 +20,11 @@ const projects = [
         "altTag": "Markdown generator app screenshot"
     },
     {
+        "urlTag": "morndusk-planner",
+        "imgRoute": "morndusk-planner-image.png",
+        "altTag": "Day planner website screenshot"
+    },
+    {
         "urlTag": "celeb-o-knowledge",
         "imgRoute": "celeb-o-knowledge-image.png",
         "altTag": "Celebration of knowledge app screenshot"
@@ -36,10 +36,10 @@ for (project in projects) {
     const projectRender = projects[project]
     console.log(projectRender);
     $('.project-cards').append(`
-        <div class="col-md-6 my-2">
+        <div class="col-md-6 my-2 ">
             <a href="https://juanwithouttheotter.github.io/${projectRender.urlTag}/">
-                <img src="../assets/images/${projectRender.imgRoute}" alt="${projectRender.altTag}"
-                    class="img-fluid" />
+                <img src="./assets/images/${projectRender.imgRoute}" alt="${projectRender.altTag}"
+                    class="img-fluid rounded border border-warning" />
             </a>
         </div>
     `);
